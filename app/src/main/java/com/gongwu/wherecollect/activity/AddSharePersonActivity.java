15c40d34248514b8c29faaed874563533bc247e8
mucll <351371167@qq.com>
2018-10-12 19:42:35 +0800

    共享人框架

--- app/src/main/java/com/gongwu/wherecollect/activity/AddSharePersonActivity.java
+++ app/src/main/java/com/gongwu/wherecollect/activity/AddSharePersonActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcel;
+import android.os.UserManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -13,11 +14,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.gongwu.wherecollect.R;
+import com.gongwu.wherecollect.adapter.AddSharePersonOldListAdapter;
+import com.gongwu.wherecollect.adapter.MyOnItemClickListener;
+import com.gongwu.wherecollect.application.MyApplication;
 import com.gongwu.wherecollect.entity.ResponseResult;
 import com.gongwu.wherecollect.entity.SharePersonBean;
 import com.gongwu.wherecollect.util.JsonUtils;
 import com.zsitech.oncon.barcode.core.CaptureActivity;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,7 +34,7 @@ import butterknife.OnClick;
 /**
  * 添加共享人
  */
-public class AddSharePersonActivity extends BaseViewActivity {
+public class AddSharePersonActivity extends BaseViewActivity implements MyOnItemClickListener {
 
     private final int START_CODE = 101;
 
@@ -39,6 +44,9 @@ public class AddSharePersonActivity extends BaseViewActivity {
     EditText addShareEditView;
     @Bind(R.id.add_share_recycler_view)
     RecyclerView mRecyclerView;
+
+    private List<SharePersonBean> datas = new ArrayList<>();
+    private AddSharePersonOldListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,21 +62,29 @@ public class AddSharePersonActivity extends BaseViewActivity {
     private void initView() {
         titleView.setText("添加共享人");
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        mAdapter = new AddSharePersonOldListAdapter(this, datas);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     private void initEvent() {
+        if (mAdapter != null) {
+            mAdapter.setOnItemClickListener(this);
+        }
     }
 
     private void initData() {
         Map<String, String> params = new HashMap<>();
+        params.put("uid", MyApplication.getUser(this).getId());
         PostListenner listener = new PostListenner(this) {
             @Override
             protected void code2000(ResponseResult r) {
                 super.code2000(r);
+                datas.clear();
                 List<SharePersonBean> beans = JsonUtils.listFromJson(r.getResult(), SharePersonBean.class);
                 if (beans != null && beans.size() > 0) {
-
+                    datas.addAll(beans);
                 }
+                mAdapter.notifyDataSetChanged();
             }
         };
         HttpClient.getAddSharePersonOldList(this, params, listener);
@@ -99,5 +115,12 @@ public class AddSharePersonActivity extends BaseViewActivity {
     public static void start(Context context) {
         Intent intent = new Intent(context, AddSharePersonActivity.class);
         context.startActivity(intent);
+    }
+
+    @Override
+    public void onItemClick(int positions, View view) {
+        if (datas != null && datas.size() >= positions) {
+            SharePersonBean bean = datas.get(positions);
+        }
     }
 }
--- app/src/main/java/com/gongwu/wherecollect/adapter/AddSharePersonOldListAdapter.java
+++ app/src/main/java/com/gongwu/wherecollect/adapter/AddSharePersonOldListAdapter.java
@@ -2,14 +2,19 @@ package com.gongwu.wherecollect.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.gongwu.wherecollect.R;
 import com.gongwu.wherecollect.entity.SharePersonBean;
+import com.gongwu.wherecollect.util.ImageLoader;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 /**
@@ -19,6 +24,11 @@ import butterknife.ButterKnife;
 public class AddSharePersonOldListAdapter extends RecyclerView.Adapter<AddSharePersonOldListAdapter.ViewHolder> {
     private Context mContext;
     private List<SharePersonBean> datas;
+    private MyOnItemClickListener onItemClickListener;
+
+    public void setOnItemClickListener(MyOnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
 
     public AddSharePersonOldListAdapter(Context mContext, List<SharePersonBean> datas) {
         this.mContext = mContext;
@@ -27,12 +37,16 @@ public class AddSharePersonOldListAdapter extends RecyclerView.Adapter<AddShareP
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_add_share_person_oldlist_layout, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        SharePersonBean bean = datas.get(position);
+        ImageLoader.loadCircle(mContext, holder.userIconIv, bean.getAvatar(), R.mipmap.ic_launcher);
+        holder.userNameTv.setText(bean.getNickname());
+        holder.userUsidTv.setText("ID: " + bean.getUsid());
     }
 
     @Override
@@ -40,11 +54,26 @@ public class AddSharePersonOldListAdapter extends RecyclerView.Adapter<AddShareP
         return datas == null ? 0 : datas.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        @Bind(R.id.add_share_person_oldlist_user_icon_iv)
+        ImageView userIconIv;
+        @Bind(R.id.add_share_person_oldlist_user_name_tv)
+        TextView userNameTv;
+        @Bind(R.id.add_share_person_oldlist_user_usid_tv)
+        TextView userUsidTv;
+
 
         public ViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (onItemClickListener != null) {
+                onItemClickListener.onItemClick(getLayoutPosition(), view);
+            }
         }
     }
 }
